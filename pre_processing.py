933187421b73abd93df1b9419cddae50c5973f30
Harsh Verma <hverma@seas.upenn.edu>
2016-11-20 22:47:51 -0500

    harsh

--- pre_processing.py
+++ pre_processing.py
@@ -13,37 +13,46 @@ import os
 
 class PreProcess:
 
-    def __init__(self, positive_dir, negative_dir):
-        self.positive_dir = positive_dir
-        self.negative_dir = negative_dir
+    def __init__(self, test_loc, train_loc):
+        self.test_loc = test_loc
+        self.train_loc = train_loc
         self.training_data = []
-        self.target = None
+        self.train_target = None
+        self.test_data = []
+        self.test_target = None
 
-    def read_train_data(self):
-        positive_files = os.listdir(self.positive_dir)
-        for filename in positive_files:
-            f = open(os.path.join(self.positive_dir, filename), 'r')
-            content = f.read()
-            self.training_data.append(content)
-        negative_files = os.listdir(self.negative_dir)
-        for filename in negative_files:
-            f = open(os.path.join(self.negative_dir, filename), 'r')
-            content = f.read()
-            self.training_data.append(content)
-        positive_labels_list = np.ones(len(positive_files))
-        negative_labels_list = np.zeros(len(negative_files))
-        self.target = np.concatenate((positive_labels_list, negative_labels_list), axis=0)
 
-    def getTrainTfIdf(self):
+    def read_train_test_data(self):
+        f_train = open(os.path.join(self.train_loc, 'reviews.txt'), 'r')
+        f_test = open(os.path.join(self.test_loc, 'reviews.txt'), 'r')
+        for line in f_train:
+            self.training_data.append(line)
+        for line in f_test:
+            self.test_data.append(line)
+        f_train = open(os.path.join(self.train_loc, 'labels.txt'), 'r')
+        f_test = open(os.path.join(self.test_loc, 'labels.txt'), 'r')
+        temp_labels_test = []
+        temp_labels_train = []
+        for line in f_train:
+            temp_labels_train.append(int(line))
+        for line in f_test:
+            temp_labels_test.append(int(line))
+        self.train_target = np.array(temp_labels_train)
+        self.test_target = np.array(temp_labels_test)
+
+
+    def getTfIdf(self):
         count_vect = CountVectorizer(stop_words='english')
         X_train_fit = count_vect.fit(self.training_data)
         X_train_counts = X_train_fit.transform(self.training_data)
         tfIdfFit = TfidfTransformer(use_idf=True, norm='l2', sublinear_tf=True).fit(X_train_counts)
         self.traintfIdf = tfIdfFit.transform(X_train_counts)
+        X_test_counts = X_train_fit.transform(self.test_data)
+        self.testtfIdf = tfIdfFit.transform(X_test_counts)
 
 
 if __name__=="__main__":
-    preprocess = PreProcess("data/pos", "data/neg")
-    preprocess.read_train_data()
-    preprocess.getTrainTestTfIdf()
+    preprocess = PreProcess("data/test", "data/train")
+    preprocess.read_train_test_data()
+    preprocess.getTfIdf()
     print "preprocessing done"
